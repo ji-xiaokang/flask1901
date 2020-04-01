dfbfa7cc41785491baeaed54cf95e476e832aca5
ji-xiaokang <1906376556@qq.com>
2020-04-01 19:08:20 +0800

    动态url

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+# @app.route('/index')
+# @app.route('/home')
+
 def index():
-    return "<h1>Hello,Flask</h1>"
+    name = "中国"
+    movies = [
+        { "title" : "大赢家" , "year" : "2020" },
+        { "title" : "叶问4" , "year" : "2020" },
+        { "title" : "唐人街探案" , "year" : "2020" },
+        { "title" : "囧妈" , "year" : "2020" },
+        { "title" : "你麻痹" , "year" : "2020" },
+    ]
+    return render_template("index.html",name=name,movies=movies)
